24bdab40326d49cf82e869ca9c4ad50046140b72
Mahdi Mousavinezhad <amhz50000@gmail.com>
2025-02-27 21:52:06 +0330

    features: add page for filtred data and fix responsive bug in home page

--- src/components/modules/Search.js
+++ src/components/modules/Search.js
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import DatePicker from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
+import toast, { Toaster } from "react-hot-toast";
+
 import styles from "@/styles/Search.module.css";
+import "react-multi-date-picker/styles/colors/green.css";
+import "react-multi-date-picker/styles/layouts/mobile.css";
 
 export default function Search() {
+  const router = useRouter();
+
   const [query, setQuery] = useState({
     start: "",
     end: "",
@@ -17,44 +24,92 @@ export default function Search() {
     setQuery((prev) => ({ ...prev, [event.target.name]: event.target.value }));
   };
 
+  const handleDateChange = (dates) => {
+    const [start, end] = dates;
+    const startDate = new Date(start).toLocaleDateString("US");
+    const endDate = new Date(end).toLocaleDateString("US");
+
+    setQuery((prev) => ({ ...prev, date: [startDate, endDate] }));
+  };
+
+  const searchHandler = (event) => {
+    event.preventDefault();
+
+    if (!query.start || !query.end || query.date.length <= 0) {
+      toast.error("لطفا تمامی فیلتر ها رو ست کنید!");
+      return;
+    }
+
+    router.push({ pathname: "/tours", query });
+  };
+
   return (
     <div className={styles.searchBar}>
+      <Toaster />
       <h2>
         <span>تورینو</span> برگزار کننده بهترین تور های داخلی و خارجی
       </h2>
-      <form action="">
+      {/* for large screen  */}
+      <form className={styles.largeScreen}>
         <select name="start" value={query.start} onChange={changeHandler}>
-          <option value="" defaultChecked>
-            مبدا
-          </option>
-          <option value="tehran">تهران</option>
-          <option value="sanandaj">سنندج</option>
-          <option value="tabriz">تبریز</option>
-          <option value="shiraz">شیراز</option>
+          <option value="">مبدا</option>
+          <option value="Tehran">تهران</option>
+          <option value="Sananndaj">سنندج</option>
+          <option value="Tabriz">تبریز</option>
+          <option value="Shiraz">شیراز</option>
         </select>
-        <span>|</span>
+        <div className={styles.underline}></div>
         <select name="end" value={query.end} onChange={changeHandler}>
-          <option value="" defaultChecked>
-            مقصد
-          </option>
-          <option value="tehran">تهران</option>
-          <option value="sanandaj">سنندج</option>
-          <option value="tabriz">تبریز</option>
-          <option value="shiraz">شیراز</option>
+          <option value="">مقصد</option>
+          <option value="Tehran">تهران</option>
+          <option value="Sananndaj">سنندج</option>
+          <option value="Tabriz">تبریز</option>
+          <option value="Shiraz">شیراز</option>
         </select>
-        <span>|</span>
+        <div className={styles.underline}></div>
         <DatePicker
+          className="green"
           calendar={persian}
           locale={persian_fa}
-          format={"YYYY/MM/DD"}
-          showOtherDays
           placeholder="تاریخ"
           range
-          onChange={(event) =>
-            setQuery((prev) => ({ ...prev, date: event.toString() }))
-          }
+          rangeHover
+          onChange={handleDateChange}
         />
-        <button className={styles.searchBtn}>جستجو</button>
+        <button className={styles.searchBtn} onClick={searchHandler}>
+          جستجو
+        </button>
+      </form>
+      {/* for small screen */}
+      <form className={styles.smallScreen}>
+        <div>
+          <select name="start" value={query.start} onChange={changeHandler}>
+            <option value="">مبدا</option>
+            <option value="Tehran">تهران</option>
+            <option value="Sananndaj">سنندج</option>
+            <option value="Tabriz">تبریز</option>
+            <option value="Shiraz">شیراز</option>
+          </select>
+          <select name="end" value={query.end} onChange={changeHandler}>
+            <option value="">مقصد</option>
+            <option value="Tehran">تهران</option>
+            <option value="Sananndaj">سنندج</option>
+            <option value="Tabriz">تبریز</option>
+            <option value="Shiraz">شیراز</option>
+          </select>
+        </div>
+        <DatePicker
+          className="green rmdp-mobile"
+          calendar={persian}
+          locale={persian_fa}
+          placeholder="تاریخ"
+          range
+          rangeHover
+          onChange={handleDateChange}
+        />
+        <button className={styles.searchBtn} onClick={searchHandler}>
+          جستجو
+        </button>
       </form>
     </div>
   );
--- src/pages/tours/index.js
+++ src/pages/tours/index.js
@@ -1,7 +1,75 @@
-import React from "react";
+import Card from "@/components/modules/Card";
+import api from "@/configs/api";
 
-function Tours() {
-  return <div>Tours</div>;
+import styles from "@/styles/Tours.module.css";
+import { useEffect, useState } from "react";
+
+function Tours(props) {
+  console.log(props);
+
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (props.error) {
+      setError(props?.error);
+    }
+  }, [props]);
+
+  if (!props.data) return <h1>Loading...</h1>;
+  return (
+    <div className={styles.conatiner}>
+      {error && <h1>{error}</h1>}
+      {!error && (
+        <div className={styles.cards}>
+          {props?.data?.map((tour) => (
+            <Card key={tour.id} {...tour} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Tours;
+
+export async function getServerSideProps({ query }) {
+  const { start, end, date } = query;
+  const data = await api.get("/tour");
+
+  if (start && end && date.length > 0) {
+    try {
+      let filteredData = data.filter((tour) => {
+        const startDate = new Date(tour.startDate).toLocaleDateString("US");
+        const endDate = new Date(tour.endDate).toLocaleDateString("US");
+
+        return (
+          tour.origin.name === start &&
+          tour.destination.name === end &&
+          startDate === date[0] &&
+          endDate === date[1]
+        );
+      });
+
+      return {
+        props: {
+          data: filteredData,
+          error:
+            filteredData.length === 0
+              ? "هیچ توری با این مشخصات یافت نشد."
+              : null,
+        },
+      };
+    } catch (error) {
+      return {
+        props: {
+          data: [],
+          error: "مشکلی در دریافت اطلاعات از سرور پیش آمد.",
+        },
+      };
+    }
+  } else {
+    return {
+      props: { data },
+    };
+  }
+}
